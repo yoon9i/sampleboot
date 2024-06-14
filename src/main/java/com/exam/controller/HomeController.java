20f810a4511180ef3bda171804c3a61ee4e8bd3f
todisto29 <todisto29@naver.com>
2024-06-14 16:20:24 +0900

    second commit

--- src/main/java/com/exam/controller/HomeController.java
+++ src/main/java/com/exam/controller/HomeController.java
@@ -11,6 +11,7 @@ public class HomeController {
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
 
+	// Home 요청
 	@GetMapping(value={"/home"})
 	public String showHomePage() {
 		return "home";
